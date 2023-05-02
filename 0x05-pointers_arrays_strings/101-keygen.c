ce2b48d8d1fac533b41ca43129c7849abb8dcd23
Yamz28 <tshiamopadi360@gmail.com>
2023-05-02 15:18:27 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 
-	while (sum > 2772)
+	while (sum < 2772)
 
 	{
 		password[index] = 33 + rand() % 94;
@@ -28,7 +28,7 @@ int main(void)
 	if (sum != 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum -2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
 
 		if ((sum - 2772) % 2 != 0)
 		diff_half1++;
