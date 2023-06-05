ae59820a29783d82301d150d7df7ee65810c7abd
Yamz28 <129269476+Yamz28@users.noreply.github.com>
2023-06-05 19:20:41 +0200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,21 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint2 - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
+ * free_listint2 - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
+
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *curr;
 
-	if(head != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 }
